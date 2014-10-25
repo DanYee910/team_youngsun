a1b1e42447a2e7112e03df1de4fe914f718bf013
Daniel Yee <yeedan@gmail.com>
2014-10-24 17:43:51 -0700

    added insults

--- source/app/controller.rb
+++ source/app/controller.rb
@@ -9,46 +9,50 @@ class Controller
     deal.check_for_ten
     deal.check_for_ace
     deal.found_blackjack
-    player_decisions
+    Controller.player_decisions(shoe, deal.player_hand)
     deal.play_house_way(shoe, deal.dealer_hand)
     deal.determine_win_lose
   end
 
-  def player_decisions
+  def self.player_decisions(shoe, hand)
     puts "What do you want to do?"
     puts "(h)it or (s)tay"
     user_input = gets.chomp.downcase
       if user_input == "h"
-        sdfasdf
+        deal_one_card(shoe, hand)
       elsif user_input == "s"
-        safsadf
+        puts "safsadf"
       else
         puts "Wrong command!"
       end
-    end
+  end
+
+  def self.insult
+    insults = File.readlines('../loser.txt')
+    puts insults.sample
   end
 end
 
 
-a = Card.new("2", 2)
-b = Card.new("7", 7)
-c = Card.new("Q", 10)
-d = Card.new("A", 11)
-e = Card.new("A", 11)
-# the_shoe = Shoe.new
-g = Deal.new(a,b,c,d,e)
-# deal_one_card(the_shoe, g.player_hand)
-# deal_one_card(the_shoe, g.dealer_hand)
-# deal_one_card(the_shoe, g.player_hand)
-# deal_one_card(the_shoe, g.dealer_hand)
+# a = Card.new("2", 2)
+# b = Card.new("7", 7)
+# c = Card.new("Q", 10)
+# d = Card.new("A", 11)
+# e = Card.new("A", 11)
+# # the_shoe = Shoe.new
+# g = Deal.new(a,b,c,d,e)
+# # deal_one_card(the_shoe, g.player_hand)
+# # deal_one_card(the_shoe, g.dealer_hand)
+# # deal_one_card(the_shoe, g.player_hand)
+# # deal_one_card(the_shoe, g.dealer_hand)
 
-g.player_hand
-g.dealer_hand
-g.check_for_ten
-g.check_for_ace
-p g.calc_hand_total(g.dealer_hand)
-p g.calc_hand_total(g.player_hand)
-p g.determine_win_lose
+# g.player_hand
+# g.dealer_hand
+# g.check_for_ten
+# g.check_for_ace
+# p g.calc_hand_total(g.dealer_hand)
+# p g.calc_hand_total(g.player_hand)
+# p g.determine_win_lose
 
-
+p Controller.insult
 
