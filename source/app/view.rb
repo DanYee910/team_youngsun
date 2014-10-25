ffd375b3da30c06cc8d3a9ae804b6f53dc4c371c
Daniel Yee <yeedan@gmail.com>
2014-10-24 17:58:58 -0700

    started view

--- source/app/view.rb
+++ source/app/view.rb
@@ -1,4 +1,7 @@
 class View
+  def self.welcome
+    puts "Welcome to Blackjack for Babies!"
+  end
 
   def self.rules
     puts rules = File.read('../rules.txt')
@@ -7,6 +10,10 @@ class View
   def self.insult
     insults = File.readlines('../loser.txt')
     puts insults.sample
+  end
+
+  def self.display_hands
+    #
   end
 
 end
